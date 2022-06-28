47ad0f2483155dd9dda758e9d6f68a582092be72
HouariZegai <houarizegai14@gmail.com>
2022-06-28 16:07:02 +0100

    feat: create first test (red)

---
+++ api/src/test/java/com/houarizegai/tennis/service/TennisGameTest.java
@@ -0,0 +1,14 @@
+package com.houarizegai.tennis.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class TennisGameTest {
+
+    @Test
+    void shouldReturnLoveAll() {
+        TennisGame tennisGame = new TennisGame();
+    }
+}
