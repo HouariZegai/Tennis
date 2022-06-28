3bed0c5082015799fdc35b5f92e584840497dd66
HouariZegai <houarizegai14@gmail.com>
2022-06-28 15:47:30 +0100

    feat: init backend project

---
+++ api/src/main/java/com/houarizegai/tennis/TennisApplication.java
@@ -0,0 +1,13 @@
+package com.houarizegai.tennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisApplication.class, args);
+    }
+
+}
