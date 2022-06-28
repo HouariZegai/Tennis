96b39993323d72f046b32d2b25cdf5d35413adc9
HouariZegai <houarizegai14@gmail.com>
2022-06-28 16:22:43 +0100

    fix: write enough prod code to make the test pass (green)

--- api/src/main/java/com/houarizegai/tennis/service/TennisGame.java
+++ api/src/main/java/com/houarizegai/tennis/service/TennisGame.java
@@ -1,11 +1,18 @@
 package com.houarizegai.tennis.service;
 
 public class TennisGame {
+
+    private int firstPlayerScore;
+
     public String getScore() {
+        if(firstPlayerScore == 1) {
+            return "Fifteen Love";
+        }
+
         return "Love All";
     }
 
     public void firstPlayerWin() {
-
+        firstPlayerScore++;
     }
 }
