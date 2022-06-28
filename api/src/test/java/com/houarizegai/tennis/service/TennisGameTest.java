95d0fa8a12407f4f2e958784ebf5db4ccc8eae6e
HouariZegai <houarizegai14@gmail.com>
2022-06-28 16:13:10 +0100

    feat: complete first failing test (red)

--- api/src/test/java/com/houarizegai/tennis/service/TennisGameTest.java
+++ api/src/test/java/com/houarizegai/tennis/service/TennisGameTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @ExtendWith(SpringExtension.class)
 class TennisGameTest {
 
@@ -11,5 +13,6 @@ class TennisGameTest {
     void shouldReturnLoveAll() {
         TennisGame tennisGame = new TennisGame();
         String score = tennisGame.getScore();
+        assertEquals("Love All", score);
     }
 }
